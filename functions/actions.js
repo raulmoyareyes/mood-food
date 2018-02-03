4f7acc5c8bbd20bc700159a596a53a18880eeb17
Daniel Ruiz <danruicar@gmail.com>
2018-02-03 19:07:17 +0100

    Added diet to API requests

--- functions/actions.js
+++ functions/actions.js
@@ -16,16 +16,15 @@ const actions = {
   },
   'input.ingredient': (request) => {
     let parameters = request.body.result.parameters;
-    // let inputContexts = request.body.result.contexts;
+    let inputContexts = request.body.result.contexts;
+    const dietContext = searchContextByName(inputContexts, 'diet');
+    const diet = dietContext.parameters.diet.replace(' ', '-');
 
-    return Http.search(parameters.ingredient).then(data => {
+    return Http.search(parameters.ingredient, diet).then(data => {
       return `Oh! I love ${parameters.ingredient}! In fact, I have found ${data.count} recipes. Tell me how many ingredients must the recipe have.`;
     });
   },
-  'input.select_diet': (request) => {
-    let parameters = request.body.result.parameters;
-    let inputContexts = request.body.result.contexts;
-    console.log('parameters:', parameters);
+  'input.select_diet': (_) => {
     return simpleMessage('I agree with your decision. Which will be the main ingredient of your meal?')
   },
   'input.ingredients_count': request => {
@@ -33,7 +32,10 @@ const actions = {
       let inputContexts = request.body.result.contexts;
       const ingredientsCount = parameters.ingredients_count;
       const ingredientContext = searchContextByName(inputContexts, 'ingredient');
-      return Http.searchWithIngredientsCount(ingredientContext.parameters.ingredient, ingredientsCount).then(data => {
+      const dietContext = searchContextByName(inputContexts, 'diet');
+      const diet = dietContext.parameters.diet.replace(' ', '-');
+
+      return Http.searchWithIngredientsCount(ingredientContext.parameters.ingredient, ingredientsCount, diet).then(data => {
           if(data && data.hits && data.hits[0]) {
             const recipe = data.hits[0].recipe;
             return `I've found your recipe! It is ${recipe.label}. Do you want me to send it to you via email?`;
